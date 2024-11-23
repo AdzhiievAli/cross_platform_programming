520027c3a821778a0c15a3ba000f65c8ac3c8888
AdzhiievAli <158584401+AdzhiievAli@users.noreply.github.com>
2024-11-23 15:48:32 +0200

    correctauth.cs

--- lab5/lab5site/Program.cs
+++ lab5/lab5site/Program.cs
@@ -1,38 +1,52 @@
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Auth0.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
 
-builder.Services.AddControllersWithViews()
-    .AddDataAnnotationsLocalization(); 
+builder.Services.AddAuth0WebAppAuthentication(options =>  
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+});
 
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseRouting(); 
 
-app.UseRouting();
+app.UseAuthentication(); 
+app.UseAuthorization(); 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    
+    endpoints.MapControllerRoute(
+        name: "registration",
+        pattern: "registration/index",
+        defaults: new { controller = "Registration", action = "Index" });
+    
+    endpoints.MapControllerRoute(
+        name: "lab1",
+        pattern: "Lab1",
+        defaults: new { controller = "Lab1", action = "Index" });
 
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
-    name: "registration",
-    pattern: "registration/index",
-    defaults: new { controller = "Registration", action = "Index" });
+            endpoints.MapControllerRoute(
+        name: "lab2",
+        pattern: "Lab2",
+        defaults: new { controller = "Lab2", action = "Index" });
+        
+            endpoints.MapControllerRoute(
+        name: "lab3",
+        pattern: "Lab3",
+        defaults: new { controller = "Lab3", action = "Index" });
+});
 
 app.Run();
